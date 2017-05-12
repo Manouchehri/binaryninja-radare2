68e72ab57c40e095518f333492dae2b5c3b01837
Sinderella <rawsec@gmail.com>
2017-05-12 10:13:31 +0100

    Wrap the function with BackgroundTaskThread
    So binja is no longer frozen during the sweep

--- __init__.py
+++ __init__.py
@@ -1,14 +1,24 @@
 from binaryninja import *
+from binaryninja.plugin import BackgroundTaskThread
 import r2pipe
 
-def spawn(bv):
-	r2p = r2pipe.open(bv.file.filename)
+class LinearSweeper(BackgroundTaskThread):
+    def __init__(self, bv):
+        BackgroundTaskThread.__init__(self, 'Linear sweeping...')
+        self.bv = bv
+
+    def run(self):
+        r2p = r2pipe.open(self.bv.file.filename)
 	r2p.cmd('aaa')
 	r2functions = r2p.cmdj('aflj')
 	r2p.quit()
 
 	for r2function in r2functions:
-		bv.add_function(r2function['offset'], plat=bv.platform)	 # should do r2function['name'] as well
-	bv.reanalyze()
+            self.bv.add_function(r2function['offset'], plat=self.bv.platform)	 # should do r2function['name'] as well
+	self.bv.reanalyze()
+
+def spawn(bv):
+    ls = LinearSweeper(bv)
+    ls.start()
 
 PluginCommand.register("radare2", "Run radare2", spawn)
