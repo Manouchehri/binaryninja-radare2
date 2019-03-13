ded73f641c8f10c993cfe22d465cfe9764546385
sheadovas <sheadovas@gmail.com>
2019-03-13 11:09:55 +0100

    Fix imported functions, add credits

--- __init__.py
+++ __init__.py
@@ -7,41 +7,34 @@ class LinearSweeper(BackgroundTaskThread):
         BackgroundTaskThread.__init__(self, 'Linear sweeping...')
         self.bv = bv
 
-    def run(self):
-        bin_filename = self.bv.file.original_filename
-
-        log.log_info("[r2] Starting analysis ({})".format(bin_filename))
-        r2p = r2pipe.open(bin_filename)
-        r2p.cmd('aaa')
-        r2functions = r2p.cmdj('aflj')
-        log.log_debug(str(r2functions))
-        r2comments = r2p.cmdj('CCj')
-        r2p.quit()
-
+    def update_functions(r2functions):
         log.log_info("[r2] Updating functions")
         for r2function in r2functions:
             addr = r2function['offset']
             bjfunc = self.bv.get_function_at(addr)
 
+            # check if detected function from radare is not created alredy
             if bjfunc is None:
-                self.bv.add_function(addr, plat=self.bv.platform) # should do r2function['name'] as well
+                self.bv.add_function(addr, plat=self.bv.platform)
+                
                 if self.bv.get_function_at(addr) is None:
                     log.log_warn('Cannot create function! Addr: 0x{:X}'.format(addr))
                     continue
 
+            # rename only if new name is not using r2 standard prefix
+            # and detected function is not recognized by binja
             bjfunc = self.bv.get_function_at(addr)
-            if 'fcn.' not in r2function['name']:
+            if 'fcn.' not in r2function['name'] and 'sub_' not in bjfunc.name:
                 log.log_info('Rename function "{}" -> "{}"'.format(
                     bjfunc.name,
                     r2function['name']
                 ))
-
                 bjfunc.name = r2function['name']
-            
         self.bv.reanalyze()
-        
+
+    def update_comments(r2comments):
+        # add radare commands
         log.log_info("[r2] Updating comments")
-        # add helper comments
         for comment in r2comments:
             addr = comment['offset']
             comm = comment['name']
@@ -55,10 +48,22 @@ class LinearSweeper(BackgroundTaskThread):
                 log.log_warn('Cannot find function! Addr: 0x{:X}, Comment: "{}"'.format(addr, comm))
 
         self.bv.reanalyze()
+
+    def run(self):
+        bin_filename = self.bv.file.original_filename
+
+        log.log_info("[r2] Starting analysis ({})".format(bin_filename))
+        r2p = r2pipe.open(bin_filename)
+        r2p.cmd('aaa')
+
+        self.update_functions(r2p.cmdj('aflj'))
+        self.update_comments(r2p.cmdj('CCj'))
+
         log.log_info("[r2] Done")
+        r2p.quit()
 
 def spawn(bv):
     ls = LinearSweeper(bv)
     ls.start()
 
-PluginCommand.register("r2 LineSweep", "Run radare2", spawn)
+PluginCommand.register("Radare2 Analysis", "Import functions and comments", spawn)
